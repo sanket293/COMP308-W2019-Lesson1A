6d4da6b52aefaabe66cb1b6e28ab2e5b1099f3f4
Sanket <scvagadiya@gmail.com>
2019-01-12 11:15:41 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+"use strict"
+(function(){
+
+
+    function Start(){
+        let variableName= 0;
+        console.log(`%cApp started...${variableName}`,"font-size: 20px; color:blue;");
+    }
+    window.addEventListener("load",Start);
+
+
+})();
